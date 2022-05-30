e6f9a4fefb400e9cf0b6251efbec66427568703e
Mohamad Farkhan <parkhan.ajah13@gmail.com>
2022-05-30 21:49:36 +0700

    Selesai Materi Events

--- app.js
+++ app.js
@@ -5,7 +5,28 @@
 // console.log(firstName);
 // console.log(lastName);
 
-const moment = require('moment');
+// const moment = require('moment');
  
-const date = moment().format("MMM Do YY");
-console.log(date);
+// const date = moment().format("MMM Do YY");
+// console.log(date);
+
+const { EventEmitter } = require('events');
+ 
+const myEventEmitter = new EventEmitter();
+ 
+const makeCoffee = (name) => {
+    console.log(`Kopi ${name} telah dibuat!`);
+};
+ 
+const makeBill = (price) => {
+    console.log(`Bill sebesar ${price} telah dibuat!`);
+}
+ 
+const onCoffeeOrderedListener = ({ name, price }) => {
+    makeCoffee(name);
+    makeBill(price);
+}
+ 
+myEventEmitter.on('coffee-order', onCoffeeOrderedListener);
+ 
+myEventEmitter.emit('coffee-order', { name: 'Tubruk', price: 15000 });
